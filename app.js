015ba5476cdfbc431b5af5773965631b12117f40
Tim Caswell <tim@c9.io>
2011-12-30 03:06:15 +0000

    Fix syntax error

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ module.exports = Stack.compose(
   Creationix.postReceive("/post-receive", Path.join(__dirname, "/post-receive.sh"), blog.warehouse),
   Creationix.vhost({"nodebits.org": function (req, res, next) {
     res.end("Coming Soon...\n");
-  }),
+  }}),
   blog
 );
 
