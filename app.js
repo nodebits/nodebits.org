b983331ce8ecd1455f6e0966c919a42bb3ef8f8d
Tim Caswell <tim@c9.io>
2011-12-30 14:37:59 +0000

    Update blog server

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ blog.warehouse();
 module.exports = Stack.compose(
   Creationix.postReceive("/post-receive", Path.join(__dirname, "/post-receive.sh"), blog.warehouse),
   Creationix.vhost({"nodebits.org": function (req, res, next) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Coming Soon...\n");
-  }}),
-  blog
+  }}, blog)
 );
 
 
