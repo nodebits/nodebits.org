2cafa783a94632598e820c64c2c814f13d3a9aa3
Tim Caswell <tim@c9.io>
2012-01-03 16:58:31 +0000

    Allow feed.xml through on the main domain

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ blog.warehouse();
 module.exports = Stack.compose(
   Creationix.postReceive("/post-receive", Path.join(__dirname, "/post-receive.sh"), blog.warehouse),
   Creationix.vhost({"nodebits.org": function (req, res, next) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Coming Soon...\n");
-  }}, blog)
+    if (req.url !== "/feed.xml") {
+      res.writeHead(200, {"Content-Type": "text/plain"});
+      res.end("Coming Soon...\n");
+    } else {
+      next();
+    }
+  }}),
+  blog
 );
 
 
