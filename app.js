92a9f9bf8a76b879259bee9b6069802441f87304
Tim Caswell <tim@creationix.com>
2012-01-04 22:39:14 -0600

    Unhide site

--- app.js
+++ app.js
@@ -8,15 +8,7 @@ blog.warehouse();
 
 module.exports = Stack.compose(
   Creationix.postReceive("/post-receive", Path.join(__dirname, "/post-receive.sh"), blog.warehouse),
-  Creationix.vhost({"nodebits.org": function (req, res, next) {
-    if (req.url !== "/feed.xml") {
-      res.writeHead(200, {"Content-Type": "text/plain"});
-      res.end("Coming Soon...\n");
-    } else {
-      next();
-    }
-  }}),
-  blog
+  Creationix.vhost({"nodebits.org": blog})
 );
 
 
